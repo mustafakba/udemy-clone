6b73d272a3670134dd978eab71f8da6c1b6c7da1
mustafakba <kaba.mustafaa@gmail.com>
2022-10-25 19:37:38 +0300

    udemy logo and left section added

--- src/components/shared/HeaderComponent.vue
+++ src/components/shared/HeaderComponent.vue
@@ -1,7 +1,18 @@
 <template>
-    <div>
-        Buraya Header Gelecek .
+    <div class="header">
+        <ul class="header-left-section flex items-center">
+            <li>
+                <img
+                    class=""
+                    style="width: 91px; height: 34px"
+                    src="../../image/logo-udemy.svg" alt="">
+            </li>
+            <li>
+                Categories
+            </li>
+        </ul>
     </div>
+
 </template>
 
 <script>
@@ -11,5 +22,9 @@ export default {
 </script>
 
 <style scoped>
-
+.header{
+    width: 100%;
+    height: 72px;
+    background-color: #F8F9FB;
+}
 </style>
