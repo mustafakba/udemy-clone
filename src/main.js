ace87b69d2d994dbf8fd5c186102559a14f74386
mustafakba <kaba.mustafaa@gmail.com>
2022-10-25 19:37:04 +0300

    tailwind-css dahil edildi

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './assets/tailwind.css'
+import VueTailwind from 'vue-tailwind'
 
 Vue.config.productionTip = false
-
+Vue.use(VueTailwind)
 new Vue({
+
   render: h => h(App),
 }).$mount('#app')
+
